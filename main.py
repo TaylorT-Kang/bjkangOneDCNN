b5037c81e369b7736aaeb994200ec83cb2c5cc8f
TaylorT <zzznm2505@gmail.com>
2020-08-14 16:12:50 +0900

    git commit

--- main.py
+++ main.py
@@ -23,9 +23,3 @@ model.load_state_dict(torch.load(PATH))
 grad_cam = gradcam.GradCam(model=model, feature_module=model.conv5, \
                     target_layer_names=["conv5"], use_cuda=DEVICE)
 
-
-## test git hub
-##sadfsfasdf
-## dafsdfdsf
-
-#dasfsf 
